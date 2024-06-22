b9c8aa66e8b11236fbf4a7251d4075a78284a169
Raziul Islam <raziul.cse@gmail.com>
2024-06-22 14:55:45 +0600

    Web push notification with `link`

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,25 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\TestController;
-
-
-
-
-
-
-
-
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use Kreait\Firebase\Contract\Messaging;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Messaging\WebPushConfig;
 
 Route::controller(HomeController::class)->group(function () {
     Route::get('/', 'index')->name('home');
-    Route::get('download-file',  'notifymessage')->name('download');
-    Route::post('firebase-storekey',  'firebase_store_key')->name('firebase.store_key');
+    Route::get('download-file', 'notifymessage')->name('download');
+    Route::post('firebase-storekey', 'firebase_store_key')->name('firebase.store_key');
 
-    Route::post('firebase-declinekey',  'firebase_decline_key')->name('firebase.decline_key');
-
+    Route::post('firebase-declinekey', 'firebase_decline_key')->name('firebase.decline_key');
 
     Route::get('offline-page.html', 'offLine')->name('offline');
     Route::get('send', 'notification_send')->name('notify');
     Route::get('git-pull', 'gitPull')->name('gitpull');
     Route::get('send-pending-notification', 'sendNotification')->name('send-pending-notification');
 });
-
 
 Route::get('login', [AuthController::class, 'index'])->name('login')->middleware('guest:admin');
 Route::post('login', [AuthController::class, 'login'])->name('login')->middleware('guest:admin');
@@ -48,4 +40,24 @@ Route::middleware(['auth:admin'])->prefix('admin')->group(function () {
     Route::get('clear-history', [DashboardController::class, 'clear_history'])->name('admin.clear_history');
 
     Route::get('logout', [AuthController::class, 'logout'])->name('admin.logout');
+});
+
+Route::get('test', function (Messaging $fcm) {
+    $token = 'dir1cK7t7F4:APA91bEbFN2tK1Qc3EEyVJfANxFN1X7jAPp8ZszldqkjBEYJjHgNaFV7giZ1lDNLDYJo-kCg0I6RGbvPwO6F2r42lEZnzUq9zoz7SB-dfR7yRAMbGEUSUBBWat-Hijo-b-T56E8Psgn4';
+
+    $webPushConfig = WebPushConfig::fromArray([
+        'notification' => [
+            'title' => __('Testing Firebase Click action for :app app', ['app' => date('Y-m-d H:i:s')]),
+            'body' => '$GOOG gained 11.80 points to close at 835.67, up 1.43% on the day.',
+            'icon' => 'https://raziul.dev/images/raziul-islam.webp',
+        ],
+        'fcm_options' => [
+            'link' => 'https://raziul.dev/simplify-slug-creation-for-eloquent-models',
+        ],
+    ]);
+
+    $message = CloudMessage::withTarget('token', $token)
+        ->withWebPushConfig($webPushConfig);
+
+    dd($fcm->send($message));
 });
